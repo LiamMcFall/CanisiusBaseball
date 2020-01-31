b590e6d93015ed3716495ced755ea3013b56d2f4
Liam McFall <limcfall7@gmail.com>
2020-01-31 10:53:59 -0500

    Commit from Rstudio

---
+++ BlastMergingCleaning.R
@@ -0,0 +1,38 @@
+library(dplyr)
+library(lubridate)
+
+# Loop through to add name column and bind the data frames
+
+aggregateBlast <- function(dataDir){
+  
+  aggregateBlastFrame <- data.frame()
+  
+  setwd(dataDir)
+  
+  fileList <- list.files()
+  
+  # Read in csv and add in name var
+  for (i in fileList) {
+    tempFrame <- read.csv(i, skip = 7)
+    nameSplit <- strsplit(i, split = " ")
+    tempFrame[,"Name"] <- nameSplit[[1]][2]
+    aggregateBlastFrame <- rbind(aggregateBlastFrame,tempFrame)
+  }
+  
+  return(aggregateBlastFrame)
+  
+}
+
+
+# Cleaning takes blast frame as parm and updates dates and names to datetime and factors respectively
+
+blastClean <- function(blastFrame){
+  
+  blastFrame$fullDate <- mdy_hms(blastTeam2019$Date)
+  blastFrame$Date <- date(blastTeam2019$fullDate)
+  blastFrame$Name <- factor(blastTeam2019$Name)
+  
+}
+
+
+
