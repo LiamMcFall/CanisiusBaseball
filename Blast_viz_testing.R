549221768215a2a9fb7811ab199d47a71f644474
Liam McFall <limcfall7@gmail.com>
2020-01-31 12:51:15 -0500

    Commit from Rstudio

--- Blast_viz_testing.R
+++ Blast_viz_testing.R
@@ -14,7 +14,7 @@ blastgg +
   geom_point()
 
 blastgg +
-  geom_encircle(s_shape=1, expand=0) +
+  geom_encircle(s_shape=.5, expand=.1) +
   geom_point()
 
 blast2019sub <- subset(blast2019, Name %in% c("bennett","leader"))
@@ -22,4 +22,5 @@ blast2019sub <- subset(blast2019, Name %in% c("bennett","leader"))
 blastsubgg <- ggplot(blast2019sub, aes(x = Bat.Speed..mph., y = Attack.Angle..deg., color = Name))
 
 blastsubgg +
-  geom_encircle(aes(fill = Name), alpha = .5)  
+  geom_encircle(aes(fill = Name), alpha = .5, expand = .01)  
+
