831dc14b383f0925915d0fcb78af6ea659b9aaf8
Liam McFall <limcfall7@gmail.com>
2020-02-02 02:22:14 -0500

    Commit from Rstudio

---
+++ BlastReport_TeamFunctions.R
@@ -0,0 +1,11 @@
+source("/Users/liammcfall/CanisiusBaseball/BlastMergingCleaning.R")
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(magrittr)
+library(ggalt)
+library(tibble)
+library(gridExtra)
+
+# Function to load the team data in order to have average to use
+teamAvg
